b02ac0a64855b78103dd7c3dcb2e688193591913
YajunWang <wangyajun@hetao101.com>
2021-07-31 15:32:12 +0800

    feat: 增加🎨

--- src/paint.rs
+++ src/paint.rs
@@ -1,3 +1,5 @@
+use std::cell::Cell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -16,7 +18,11 @@ pub fn run() -> Result<(), JsValue> {
         .create_element("canvas")?
         .dyn_into::<web_sys::HtmlCanvasElement>()?;
 
-    document.body().unwrap().append_child(&canvas);
+    canvas.set_width(640);
+    canvas.set_height(640);
+    canvas.style().set_property("border", "solid")?;
+
+    document.body().unwrap().append_child(&canvas)?;
 
     let context = canvas
         .get_context("2d")
@@ -25,15 +31,54 @@ pub fn run() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    canvas.set_width(640);
-    canvas.set_height(640);
+    let context = Rc::new(context);
+    let pressed = Rc::new(Cell::new(false));
 
-    context.set_fill_style(&"rgb(150,50,0)".into());
-    context.fill_rect(0.0, 0.0, 200.0, 200.0);
-    context.set_fill_style(&"rgb(0, 150, 150)".into());
-    context.fill_rect(10.0, 10.0, 180.0, 180.0);
-    context.set_fill_style(&"rgb(150, 0, 150)".into());
-    context.fill_rect(20.0, 20.0, 160.0, 160.0);
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            context.begin_path();
+            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            pressed.set(true);
+
+            // context.set_fill_style(&"rgb(150,50,0)".into());
+            // context.fill_rect(0.0, 0.0, 100.0, 100.0);
+            // context.set_fill_style(&"rgb(0, 150, 150)".into());
+            // context.fill_rect(10.0, 10.0, 80.0, 80.0);
+            // context.set_fill_style(&"rgb(150, 0, 150)".into());
+            // context.fill_rect(20.0, 20.0, 60.0, 60.0);
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            if (pressed.get()) {
+                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+                context.stroke();
+                context.begin_path();
+                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            }
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            pressed.set(false);
+            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+            context.stroke();
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
 
     Ok(())
 }
